6749b308b3819840a390158c71b52d3cd22e0d8a
Matheus <matheustorresdev@gmail.com>
2023-12-26 19:02:42 -0300

    Finalizando tela de SignIn

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,21 +2,28 @@ import { Button as ButtonNativeBase, IButtonProps, Text } from "native-base";
 
 type Props = IButtonProps & {
   title: string;
+  variant?: "solid" | "outline";
 };
 
-export const Button = ({ title, ...props }: Props) => {
+export const Button = ({ title, variant = "solid", ...props }: Props) => {
   return (
     <ButtonNativeBase
       w="full"
       h={14}
-      bg="green.700"
+      bg={variant === "outline" ? "transparent" : "green.700"}
+      borderWidth={variant === "outline" ? 1 : 0}
+      borderColor="green.500"
       rounded="sm"
       _pressed={{
         bg: "green.500",
       }}
       {...props}
     >
-      <Text color="white" fontFamily="heading" fontSize="sm">
+      <Text
+        color={variant === "outline" ? "green.500" : "white"}
+        fontFamily="heading"
+        fontSize="sm"
+      >
         {title}
       </Text>
     </ButtonNativeBase>
