09e59db27abfba30f67c99fa77aa32e82ad8f8f2
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-12-26 12:58:20 -0300

    Componente de Input

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -2,6 +2,7 @@ import { VStack, Image, Center, Text, Heading } from "native-base";
 
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
+import { Input } from "@components/Input";
 
 export const SignIn = () => {
   return (
@@ -25,6 +26,9 @@ export const SignIn = () => {
         <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
           Acesse a conta
         </Heading>
+
+        <Input placeholder="E-mail" />
+        <Input placeholder="Senha" />
       </Center>
     </VStack>
   );
