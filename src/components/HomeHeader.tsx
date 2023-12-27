f6d1d3bec02e6df9921b93722e1025b8bf327bc2
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-12-27 08:21:18 -0300

    Criando Componente UserPhoto

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,15 @@
 import { HStack, Heading, Text, VStack } from "native-base";
+import { UserPhoto } from "./UserPhoto";
 
 export const HomeHeader = () => {
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+        <UserPhoto 
+        source={{uri: "https://github.com/matheustorresdev97.png"}}
+        size={16}
+        alt="Imagem do usuário"
+        mr={4}
+        />
       <VStack>
         <Text color="gray.100" fontSize="md">
           Olá,
