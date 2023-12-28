8b6a697740fa0da2c420c98d177a228881fe9857
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-12-28 08:32:46 -0300

    Componente HistoryCard

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,11 +1,17 @@
-import { ScreenHeader } from "@components/ScreenHeader";
 import { VStack } from "native-base";
+
+import { HistoryCard } from '@components/HistoryCard';
+import { ScreenHeader } from '@components/ScreenHeader';
 
 
 export const History = () => {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico" />
+
+      <HistoryCard />
+      <HistoryCard />
+      <HistoryCard />
     </VStack>
   );
 };
