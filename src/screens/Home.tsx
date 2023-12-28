47992011f53bb336809c5fa3191ef061634ce16d
Matheus <matheustorresdev@gmail.com>
2023-12-27 21:09:52 -0300

    Header da lista de exercicios

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FlatList, HStack, VStack } from "native-base";
+import { FlatList, HStack, Heading, Text, VStack } from "native-base";
 
 import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
@@ -34,6 +34,18 @@ export const Home = () => {
         my={10}
         maxH={10}
       />
+
+      <VStack px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   );
 };
