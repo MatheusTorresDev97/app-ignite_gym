81c9e0c7edf93b65e59f33984ca6067ae3df4a22
Matheus <matheustorresdev@gmail.com>
2023-12-27 21:02:39 -0300

    Finalizando Componente Group e exibindo lista

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,39 @@
-import { VStack } from "native-base";
+import { useState } from "react";
+import { FlatList, HStack, VStack } from "native-base";
 
 import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
 
 export const Home = () => {
+  const [groupSelected, setGroupSelected] = useState("Costas");
+  const [groups, setGroups] = useState([
+    "Costas",
+    "Bíceps",
+    "Tríceps",
+    "Ombro",
+  ]);
   return (
     <VStack flex={1}>
       <HomeHeader />
 
-      <Group name="Costas" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 8,
+        }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   );
 };
